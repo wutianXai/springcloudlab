d22acc680905a2aa350b9039ca7e6409515baa66
wutianXai <546895488@qq.com>
2020-09-07 21:58:20 +0800

    单机 eureka

--- payment/src/main/java/com/wutian/springcloud/PaymentMain01.java
+++ payment/src/main/java/com/wutian/springcloud/PaymentMain01.java
@@ -3,10 +3,12 @@ package com.wutian.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Telson
  */
+@EnableEurekaClient
 @SpringBootApplication
 @MapperScan("com.wutian.springcloud.dao")
 public class PaymentMain01{
